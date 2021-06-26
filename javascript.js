612509dcaabb91709141aeeacd9ed3eb5f38ca45
dimahalaiskiy <dgalaiskiy@icloud.com>
2021-06-26 11:23:58 +0300

    benefits-1.1

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+
+
+let name = (a) => {
+    console.log(arguments[0])
+    console.log(arguments[1])
+    console.log(arguments[2])
+    console.log(arguments.length)
+}
+
+name('a', 'b', 'c','d');
